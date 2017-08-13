7aedbbea9e5d911dd91950ad15f71e38a1efa6b0
sponyzhang <sponyzhang@126.com>
2017-08-13 11:57:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require('express')
 const http = require('http')
 const app = express()
 
-// 跨域设置
-// app.all('*', function (req, res, next) {
-//   res.header("Access-Control-Allow-Credentials", true)
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With")
-//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
-//   res.header("X-Powered-By", ' 3.2.1')
-//   res.header("Content-Type", "application/json;charset=utf-8")
-//   next()
-// })
+//跨域设置
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8")
+  next()
+})
 
 app.use(express.static('public'));
 
